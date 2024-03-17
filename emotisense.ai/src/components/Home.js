a238863aa88f14e54e3a3bfa5f3131d293401679
Haris Khawja <92524990+hariskhawja@users.noreply.github.com>
2024-03-17 15:14:10 -0400

    feat: animations and upload

--- emotisense.ai/src/components/Home.js
+++ emotisense.ai/src/components/Home.js
@@ -9,6 +9,7 @@ const Home = () => {
 
   function handleChange(event) {
     setFile(event.target.files[0]);
+
   }
 
   function handleSubmit(event) {
@@ -43,12 +44,11 @@ const Home = () => {
             <div className="hero-content">
                 <div className='title'>EmotionSense<span style={{color: "rgb(0, 0, 139)", fontWeight: "bold"}}>.AI</span></div>
                 
-            <form onSubmit={handleSubmit}>
-                <div class="input-div">
-
-                <input class="input" name="file" type="file" accept="json" onChange={handleChange}></input>
-                    {/* <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" stroke-linejoin="round" stroke-linecap="round" viewBox="0 0 24 24" stroke-width="2" fill="none" stroke="currentColor" class="icon-upload"><polyline points="16 16 12 12 8 16"></polyline><line y2="21" x2="12" y1="12" x1="12"></line><path d="M20.39 18.39A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.3"></path><polyline points="16 16 12 12 8 16"></polyline></svg> */}
-                
+            <form className="form-container" onSubmit={handleSubmit}>
+                <div className="input-div">
+                <input className="input" name="file" type="file" accept="json" onChange={handleChange}></input>
+                    {!file && <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" stroke-linejoin="round" stroke-linecap="round" viewBox="0 0 24 24" stroke-width="2" fill="none" stroke="currentColor" class="icon-upload"><polyline points="16 16 12 12 8 16"></polyline><line y2="21" x2="12" y1="12" x1="12"></line><path d="M20.39 18.39A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.3"></path><polyline points="16 16 12 12 8 16"></polyline></svg>}
+                    {file && <p className="file-selected">{file.name}</p>}
                 </div>
                 <button type='submit' className='button-submit'>Submit</button>
             </form>
